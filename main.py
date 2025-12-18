5e8cb1feee05993216473d1bba4b8855aaed1037
bmonagan <bernardmonagan@gmail.com>
2025-12-18 13:03:02 -0500

    added in functions for size checks

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import sys
 
 args = sys.argv
 
-flags = []
+flags = set()
 paths = []
 supported_flags = "cml"
 supported_flagsL = ["chars","bytes","lines"]
-
+def get_bytes(content):
+    return sys.getsizeof(content)
+def new_lines(content):
+    return text.count("\n")
+def char_count(content):
+    return len(content)
 for arg in args[1:]:
     if len(arg) == 2 and arg[0] == "-" and arg[1:].lower() in supported_flags:
-        flags.append(arg)
+        flags.add(arg)
     elif len(arg) > 2 and arg[0:2] == "--" and arg[2:].lower() in supported_flagsL:
-        flags.append(arg)
+        flags.add(arg)
     else:
         paths.append(arg)
 if not flags and not paths:
